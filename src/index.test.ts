9d4c9325f4b0e40c0c167f348ef6ea96313f304e
Rahul Mehta <mehta.rahul95@gmail.com>
2021-10-04 12:29:19 -0400

    chore: add eslint

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,57 +1,55 @@
-import test from 'ava'
 import process from 'process';
-import {path as appRootPath} from 'app-root-path';
+import test from 'ava';
+import { path as appRootPath } from 'app-root-path';
 import { exec } from './util/exec';
 const TEST_ROOT = `${appRootPath}/integration-tests`;
 
-
-test.before(async t => {
-  process.chdir(appRootPath);
+test.before(async () => {
+    process.chdir(appRootPath);
 });
 
-test.after(async t => {
-  process.chdir(appRootPath);
-  exec('./integration-tests/cleanup_all.sh')
+test.after(async () => {
+    process.chdir(appRootPath);
+    exec('./integration-tests/cleanup_all.sh');
 });
 
-test.serial('can detect android file change', async t => {
-  const testDir = `${TEST_ROOT}/android-bump`;
-  console.log(`Setting workdir to ${testDir}`);
-  process.chdir(testDir);
+test.serial('can detect android file change', async (t) => {
+    const testDir = `${TEST_ROOT}/android-bump`;
+    console.log(`Setting workdir to ${testDir}`);
+    process.chdir(testDir);
 
-  await exec('./runTest.sh');
-  const {stdout, stderr} = await exec('npx native-modules-lock check');
-  if (stderr == '') {
-    t.fail(stderr);
-  } else {
-    t.pass(stdout);
-  }
+    await exec('./runTest.sh');
+    const { stdout, stderr } = await exec('npx native-modules-lock check');
+    if (stderr == '') {
+        t.fail(stderr);
+    } else {
+        t.pass(stdout);
+    }
 });
 
-test.serial('can detect ios file change', async t => {
-  const testDir = `${TEST_ROOT}/ios-bump`;
-  console.log(`Setting workdir to ${testDir}`);
-  process.chdir(testDir);
+test.serial('can detect ios file change', async (t) => {
+    const testDir = `${TEST_ROOT}/ios-bump`;
+    console.log(`Setting workdir to ${testDir}`);
+    process.chdir(testDir);
 
-  await exec('./runTest.sh');
-  const {stdout, stderr} = await exec('npx native-modules-lock check');
-  if (stderr == '') {
-    t.fail(stderr);
-  } else {
-    t.pass(stdout);
-  }
+    await exec('./runTest.sh');
+    const { stdout, stderr } = await exec('npx native-modules-lock check');
+    if (stderr == '') {
+        t.fail(stderr);
+    } else {
+        t.pass(stdout);
+    }
 });
 
-test.serial('can detect unchanged state', async t => {
-  const testDir = `${TEST_ROOT}/unchanged-lock`;
-  console.log(`Setting workdir to ${testDir}`);
-  process.chdir(testDir);
+test.serial('can detect unchanged state', async (t) => {
+    const testDir = `${TEST_ROOT}/unchanged-lock`;
+    console.log(`Setting workdir to ${testDir}`);
+    process.chdir(testDir);
 
-  const {stdout, stderr} = await exec('npx native-modules-lock check');
-  if (stderr != '') {
-    t.fail(stderr);
-  } else {
-    t.pass(stdout);
-  }
-
+    const { stdout, stderr } = await exec('npx native-modules-lock check');
+    if (stderr != '') {
+        t.fail(stderr);
+    } else {
+        t.pass(stdout);
+    }
 });
--- src/util/fs.ts
+++ src/util/fs.ts
@@ -1,20 +1,24 @@
-import fs from 'fs-extra';
 import { promisify } from 'util';
-import { exec } from "./exec";
+import fs from 'fs-extra';
+import { exec } from './exec';
 
 export type Path = fs.PathLike;
 
-export const readFile: (arg1: number | fs.PathLike) => Promise<Buffer> = promisify(fs.readFile);
+export const readFile: (arg1: number | fs.PathLike) => Promise<Buffer> =
+    promisify(fs.readFile);
 
 export async function listDir(dir: Path): Promise<Path[]> {
     const command = `git ls-files ${dir}`;
     return exec(command).then(({ stdout }) => {
         const paths = stdout.split('\n');
-        return paths.slice(0, paths.length-1);
+        return paths.slice(0, paths.length - 1);
     });
 }
 
-export async function mapFile<T>(path: Path, callback: (buffer: Buffer) => T): Promise<T> {
+export async function mapFile<T>(
+    path: Path,
+    callback: (buffer: Buffer) => T
+): Promise<T> {
     return readFile(path).then((value) => {
         return callback(value);
     });
